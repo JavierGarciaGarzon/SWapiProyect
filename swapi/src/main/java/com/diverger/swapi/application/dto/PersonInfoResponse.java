10fb06a3ef52493be0086bd98fab020cfb1f56a8
jggarzon <javiergarciagarzon@gmail.com>
2024-08-23 17:54:28 +0200

    Add response

---
+++ swapi/src/main/java/com/diverger/swapi/application/dto/PersonInfoResponse.java
@@ -0,0 +1,19 @@
+package com.diverger.swapi.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonInfoResponse {
+    private String name;
+    private String birthYear;
+    private String gender;
+    private String planetName;
+    private String fastestVehicleDriven;
+    private List<FilmResponse> films;
+}
