964c729342b8227a7ed56fb9789a5f1efe4b0e8a
jggarzon <javiergarciagarzon@gmail.com>
2024-08-25 16:15:43 +0200

    Add custom errors classes

---
+++ swapi/src/main/java/com/diverger/swapi/application/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.diverger.swapi.application.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String details;
+    private String message;
+    private LocalDateTime timestamp;
+}
---
+++ swapi/src/main/java/com/diverger/swapi/application/exception/PersonNotFoundException.java
@@ -0,0 +1,9 @@
+package com.diverger.swapi.application.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class PersonNotFoundException extends SwapiException{
+    public PersonNotFoundException(String message) {
+        super(message, HttpStatus.NOT_FOUND);
+    }
+}
